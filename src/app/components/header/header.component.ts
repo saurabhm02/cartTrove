0964f132c36916d5dbcdd372c25ef3426c3700d5
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-05 03:14:19 +0530

    header component added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  public totalItem : number = 0;
+  constructor(private cartService : CartService) { }
+  
+  ngOnInit(): void {
+    this.cartService.getProduct()
+    .subscribe(res=>{
+      this.totalItem = res.length;
+    })
+  }
 }
