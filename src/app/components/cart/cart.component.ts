4800922c9215067b7adb9d2d3d312616ddc21b6c
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-05 03:13:59 +0530

    cart component added

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
 
+  public product : any=[];
+  public grandTotal : number = 0;
+
+  constructor(private cartService : CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getProduct()
+    .subscribe(res => {
+      this.product = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    })
+  }
+  removeItem(item : any){
+    this.cartService.removeCartItem(item);
+  }
+  emptyCart(){
+    this.cartService.removeAllCart();
+  }
 }
