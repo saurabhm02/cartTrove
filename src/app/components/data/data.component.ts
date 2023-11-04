3c01c58c9ee66e3f4631f4eedd6457a319f9842a
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-05 03:13:35 +0530

    data/ products component added

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-data',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./data.component.scss']
 })
 export class DataComponent {
+  public productList : any;
+  constructor(private api : ApiService, private CartService : CartService) { }
+  
+  ngOnInit() :void{
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
 
+      this.productList.forEach((a:any) => {
+        Object.assign(a, {quantity:1, total:a.price});
+      });
+    })
+  }
+
+  addToCart(post: any){
+    this.CartService.addToCart(post);
+  }
 }
