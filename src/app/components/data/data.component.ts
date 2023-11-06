0e59743c5d718366a32288cc883d00962d698f44
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-06 21:12:25 +0530

    product component updated

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -8,21 +8,40 @@ import { CartService } from 'src/app/service/cart.service';
   styleUrls: ['./data.component.scss']
 })
 export class DataComponent {
+  selectedCategory: string = '';
   public productList : any;
+  public filterCategory : any;
+  searchKey : string = "";
   constructor(private api : ApiService, private CartService : CartService) { }
   
   ngOnInit() :void{
     this.api.getProduct()
     .subscribe(res=>{
       this.productList = res;
-
+      this.filterCategory = res;
       this.productList.forEach((a:any) => {
+        if(a.category === "men's clothing" || a.category === "women's clothing"){
+          a.category = "fashion"
+        }
         Object.assign(a, {quantity:1, total:a.price});
       });
+    });
+    this.CartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
     })
   }
 
   addToCart(post: any){
     this.CartService.addToCart(post);
   }
+
+  filter(category : string){
+    this.selectedCategory = category;
+    this.filterCategory = this.productList
+    .filter((a:any)=>{
+      if(a.category == category || category == ''){
+        return a;
+      }
+  })
+}
 }
