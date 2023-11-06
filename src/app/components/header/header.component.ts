6001f7a47767fbab93d6d9b82523137668ad0842
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-06 21:12:59 +0530

    added some functionality in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,6 +9,7 @@ import { CartService } from 'src/app/service/cart.service';
 export class HeaderComponent implements OnInit {
 
   public totalItem : number = 0;
+  public searchTerm !: string;
   constructor(private cartService : CartService) { }
   
   ngOnInit(): void {
@@ -16,5 +17,11 @@ export class HeaderComponent implements OnInit {
     .subscribe(res=>{
       this.totalItem = res.length;
     })
+  }
+
+  search(event:any){
+    this.searchTerm = (event.target as HTMLInputElement).value;
+    console.log(this.searchTerm);
+    this.cartService.search.next(this.searchTerm);
   }
 }
