9154f021c024ed30295c49a4a5923e4dadb7583f
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-17 01:21:41 +0530

    updated  header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,93 @@
+// import { Component, OnInit } from '@angular/core';
+// import { CartService } from 'src/app/service/cart.service';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.scss']
+// })
+// export class HeaderComponent implements OnInit {
+
+//   public totalItem : number = 0;
+//   public searchTerm !: string;
+//   constructor(private cartService : CartService) { }
+  
+//   ngOnInit(): void {
+//     this.cartService.getProduct()
+//     .subscribe(res=>{
+//       this.totalItem = res.length;
+//     })
+//   }
+
+//   search(event:any){
+//     this.searchTerm = (event.target as HTMLInputElement).value;
+//     console.log(this.searchTerm);
+//     this.cartService.search.next(this.searchTerm);
+//     console.log(this.cartService.search.next(this.searchTerm));
+//   }
+// }
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { CartService } from 'src/app/service/cart.service';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.scss']
+// })
+// export class HeaderComponent implements OnInit {
+
+//   public totalItem: number = 0;
+//   public searchTerm!: string;
+
+//   constructor(private cartService: CartService) { }
+  
+//   ngOnInit(): void {
+//     this.cartService.getProduct().subscribe(res => {
+//       this.totalItem = res.length;
+//     });
+//   }
+
+//   search(event: any) {
+//     this.searchTerm = (event.target as HTMLInputElement).value;
+//     console.log(this.searchTerm);
+//     this.cartService.search.next(this.searchTerm);
+//   }
+// }
+
+
+// header.component.ts
+
+// import { Component, OnInit } from '@angular/core';
+// import { CartService } from 'src/app/service/cart.service';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.scss'],
+// })
+// export class HeaderComponent implements OnInit {
+//   public totalItem: number = 0;
+//   public searchTerm!: string;
+
+//   constructor(private cartService: CartService) {}
+
+//   ngOnInit(): void {
+//     // Subscribe to changes in the productList observable
+//     this.cartService.getProduct().subscribe((res) => {
+//       // Update totalItem based on the length of the received array
+//       this.totalItem = res.length;
+//     });
+//   }
+
+//   search(event: any) {
+//     this.searchTerm = (event.target as HTMLInputElement).value;
+//     console.log('Search Term:', this.searchTerm);
+//     this.cartService.search.next(this.searchTerm);
+//   }
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/service/cart.service';
 
@@ -8,24 +98,35 @@ import { CartService } from 'src/app/service/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  public totalItem : number = 0;
-  public searchTerm !: string;
-  isSearchInputVisible: boolean = false;
-  constructor(private cartService : CartService) { }
+  public totalItem: number = 0;
+  public searchTerm: string = '';
+  public filteredItems: any[] = [];
+
+  constructor(private cartService: CartService) { }
 
   toggleSearchInput() {
-    this.isSearchInputVisible = !this.isSearchInputVisible;
-  }
-  ngOnInit(): void {
-    this.cartService.getProduct()
-    .subscribe(res=>{
-      this.totalItem = res.length;
-    })
+    // Toggle your search input visibility logic here if needed
   }
 
-  search(event:any){
+  ngOnInit(): void {
+    this.cartService.getProduct()
+      .subscribe(res => {
+        this.totalItem = res.length;
+      });
+
+      this.cartService.filteredProductList.subscribe(filteredItems => {
+        this.filteredItems = filteredItems;
+      });
+  }
+
+  search(event: any) {
     this.searchTerm = (event.target as HTMLInputElement).value;
-    console.log(this.searchTerm);
+    console.log('Search Term:', this.searchTerm);
+    
+    // Update the searchTerm in the cart service
     this.cartService.search.next(this.searchTerm);
+
+    // Call the filterProducts method in the cart service
+    this.cartService.filterProducts(this.searchTerm);
   }
 }
