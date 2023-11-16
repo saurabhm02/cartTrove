4b4c1d310fd9b62b4a4697e9e3799826baac23a2
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-17 01:22:00 +0530

    updated cart service

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,3 +1,158 @@
+// // import { Injectable } from '@angular/core';
+// // import { BehaviorSubject } from 'rxjs';
+
+// // @Injectable({
+// //   providedIn: 'root'
+// // })
+// // export class CartService {
+// //   public cartItemList : any=[]
+// //   public productList = new BehaviorSubject<any>([]);
+// //   public search = new BehaviorSubject<string>("");
+
+// //   constructor() { }
+
+// //   getProduct(){
+// //     return this.productList.asObservable();
+// //   }
+
+// //   setProduct(product : any){
+// //     this.cartItemList.push(...product);
+// //     this.productList.next(product);
+// //   }
+
+// //   addToCart(product: any){
+// //     this.cartItemList.push(product);
+// //     this.productList.next(this.cartItemList);
+// //     this.getTotalPrice();
+// //     console.log(this.cartItemList);
+// //   }
+
+// //   getTotalPrice() : number{
+// //     let grandTotal = 0;
+// //     this.cartItemList.map((a:any)=>{
+// //       grandTotal += a.total;
+// //     })
+// //     return grandTotal;
+// //   }
+// //   removeCartItem(product: any){
+// //     this.cartItemList.map((a:any, index:any)=>{
+// //       if(product.id === a.id){
+// //         this.cartItemList.splice(index,1);
+// //       }
+// //     })
+// //     this.productList.next(this.cartItemList);
+// //   }
+// //   removeAllCart(){
+// //     this.cartItemList = []
+// //     this.productList.next(this.cartItemList);
+// //   }
+// // }
+
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class CartService {
+//   public cartItemList : any=[]
+//   public productList = new BehaviorSubject<any>([]);
+//   public search = new BehaviorSubject<string>("");
+
+//   constructor() { }
+
+//   getProduct(){
+//     return this.productList.asObservable();
+//   }
+
+//   setProduct(product : any){
+//     this.cartItemList.push(...product);
+//     this.productList.next(product);
+//   }
+
+//   addToCart(product: any){
+//     this.cartItemList.push(product);
+//     this.productList.next(this.cartItemList);
+//     this.getTotalPrice();
+//     console.log(this.cartItemList);
+//   }
+
+//   getTotalPrice() : number{
+//     let grandTotal = 0;
+//     this.cartItemList.map((a:any)=>{
+//       grandTotal += a.total;
+//     })
+//     return grandTotal;
+//   }
+//   removeCartItem(product: any){
+//     this.cartItemList.map((a:any, index:any)=>{
+//       if(product.id === a.id){
+//         this.cartItemList.splice(index,1);
+//       }
+//     })
+//     this.productList.next(this.cartItemList);
+//   }
+//   removeAllCart(){
+//     this.cartItemList = []
+//     this.productList.next(this.cartItemList);
+//   }
+// }
+
+
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class CartService {
+//   public cartItemList: any = [];
+//   public productList = new BehaviorSubject<any>([]);
+//   public search = new BehaviorSubject<string>("");
+
+//   constructor() { }
+
+//   getProduct() {
+//     return this.productList.asObservable();
+//   }
+
+//   setProduct(product: any) {
+//     this.cartItemList.push(...product);
+//     this.productList.next(this.cartItemList);
+//   }
+
+//   addToCart(product: any) {
+//     this.cartItemList.push(product);
+//     this.productList.next(this.cartItemList);
+//     this.getTotalPrice();
+//     console.log(this.cartItemList);
+//   }
+
+//   getTotalPrice(): number {
+//     let grandTotal = 0;
+//     this.cartItemList.map((a: any) => {
+//       grandTotal += a.total;
+//     });
+//     return grandTotal;
+//   }
+
+//   removeCartItem(product: any) {
+//     this.cartItemList.map((a: any, index: any) => {
+//       if (product.id === a.id) {
+//         this.cartItemList.splice(index, 1);
+//       }
+//     });
+//     this.productList.next(this.cartItemList);
+//   }
+
+//   removeAllCart() {
+//     this.cartItemList = [];
+//     this.productList.next(this.cartItemList);
+//   }
+// }
+
+
+
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -5,45 +160,54 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
-  public cartItemList : any=[]
+  public cartItemList: any[] = [];
   public productList = new BehaviorSubject<any>([]);
-  public search = new BehaviorSubject<string>("");
+  public search = new BehaviorSubject<string>('');
+  public filteredProductList = new BehaviorSubject<any[]>([]);
 
   constructor() { }
 
-  getProduct(){
+  getProduct() {
     return this.productList.asObservable();
   }
 
-  setProduct(product : any){
+  setProduct(product: any) {
     this.cartItemList.push(...product);
     this.productList.next(product);
   }
 
-  addToCart(product: any){
+  addToCart(product: any) {
     this.cartItemList.push(product);
     this.productList.next(this.cartItemList);
     this.getTotalPrice();
-    console.log(this.cartItemList);
   }
 
-  getTotalPrice() : number{
+  getTotalPrice(): number {
     let grandTotal = 0;
-    this.cartItemList.map((a:any)=>{
+    this.cartItemList.forEach((a: any) => {
       grandTotal += a.total;
-    })
+    });
     return grandTotal;
   }
-  removeCartItem(product: any){
-    this.cartItemList.map((a:any, index:any)=>{
-      if(product.id === a.id){
-        this.cartItemList.splice(index,1);
-      }
-    })
-    this.productList.next(this.cartItemList);
+
+  removeCartItem(product: any) {
+    const index = this.cartItemList.findIndex((a: any) => product.id === a.id);
+    if (index !== -1) {
+      this.cartItemList.splice(index, 1);
+      this.productList.next(this.cartItemList);
+    }
   }
-  removeAllCart(){
-    this.cartItemList = []
+
+  filterProducts(searchTerm: string) {
+    // Implement your filtering logic here
+    const filteredItems = this.cartItemList.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    
+    // Update the filteredProductList BehaviorSubject
+    this.filteredProductList.next(filteredItems);
+  }
+
+  removeAllCart() {
+    this.cartItemList = [];
     this.productList.next(this.cartItemList);
   }
 }
