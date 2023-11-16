f37ffbc105e22e5e147269980554745d41c1cf30
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-17 01:19:26 +0530

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,42 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { HeaderComponent } from './components/header/header.component';
+// import { CartComponent } from './components/cart/cart.component';
+// import { DataComponent } from './components/data/data.component';
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { FilterPipe } from './shared/filter.pipe';
+// import { LandingpageComponent } from './components/landingpage/landingpage.component';
+// import { FooterComponent } from './components/footer/footer.component';
+// import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HeaderComponent,
+//     CartComponent,
+//     DataComponent,
+//     FilterPipe,
+//     LandingpageComponent,
+//     FooterComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     HttpClientModule,
+//     NgbModule,
+//     FormsModule,
+//     NgxSkeletonLoaderModule,
+//     ReactiveFormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
