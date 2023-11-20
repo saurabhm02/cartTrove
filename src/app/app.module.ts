3a188ec44f00684f34f30057e8c99e2646541c83
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-20 22:52:45 +0530

    updated app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { HeaderComponent } from './components/header/header.component';
-// import { CartComponent } from './components/cart/cart.component';
-// import { DataComponent } from './components/data/data.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { FilterPipe } from './shared/filter.pipe';
-// import { LandingpageComponent } from './components/landingpage/landingpage.component';
-// import { FooterComponent } from './components/footer/footer.component';
-// import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     HeaderComponent,
-//     CartComponent,
-//     DataComponent,
-//     FilterPipe,
-//     LandingpageComponent,
-//     FooterComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule,
-//     NgbModule,
-//     FormsModule,
-//     NgxSkeletonLoaderModule,
-//     ReactiveFormsModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -51,6 +12,8 @@ import { FilterPipe } from './shared/filter.pipe';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -69,7 +32,9 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
     NgbModule,
     FormsModule,
     NgxSkeletonLoaderModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
