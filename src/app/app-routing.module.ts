8575f57d2ee8dc77e6879dc7ba63602e483baae4
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-25 17:16:01 +0530

    add some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DataComponent } from './components/data/data.component';
 import { CartComponent } from './components/cart/cart.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { CheckoutPageComponent } from './components/checkout-page/checkout-page.component';
+import { CompleteOrderComponent } from './components/complete-order/complete-order.component';
 
 const routes: Routes = [
   {path:'', component: LandingpageComponent },
   {path:'products', component:DataComponent},
-  {path: 'cart', component:CartComponent}
+  {path: 'cart', component:CartComponent},
+  {path: 'checkout', component:CheckoutPageComponent},
+  {path: 'orderSuccess', component:CompleteOrderComponent}
 ];
 
 @NgModule({
